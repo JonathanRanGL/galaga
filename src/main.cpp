e8f09c195b17fc6527a549a81d48ed82ec3fb2c8
JonathanRanGL <159947420+JonathanRanGL@users.noreply.github.com>
2024-06-13 03:00:36 -0600

    Ajustes iniciales para comenzar a trabajar
    Se eliminaron unas cosas en el proceso en el que aprendemos como trabajar en este entorno, mayormente quitar cosas usadas en anterior librería gráfica.

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,44 @@
-#include <iostream>
+#include <SFML/Graphics.hpp>
 
-int main(int argc, char const *argv[])
+int main()
 {
-    std::cout << "Probando" << std::endl;
+    // Crear una ventana de SFML
+    sf::RenderWindow window(sf::VideoMode(768, 1024), "SFML works!",sf::Style::Titlebar | sf::Style::Close);
+    
+
+    // Crear una forma circular de SFML
+    sf::CircleShape shape(100.f);
+    shape.setFillColor(sf::Color::Red);
+    sf::Event event;
+
+    while (window.isOpen())
+    {
+        while(window.pollEvent(event))
+        {
+            switch(event.type)
+            {
+                case sf::Event::Closed:
+                    window.close();
+                    break;
+                
+                case sf::Event::KeyPressed:
+                    if(event.key.code == sf::Keyboard::Escape){window.close();}
+                    break;
+
+            }
+        }
+
+        
+
+        // Renderizar
+        window.clear();
+
+        // Dibujar la forma en la ventana
+        window.draw(shape);
+
+        // Mostrar la ventana
+        window.display();
+    }
+
     return 0;
 }
